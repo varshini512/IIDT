6d24bae46a5ffbbcb6f7854c03bb57496bcaf8eb
varshini512 <144098661+varshini512@users.noreply.github.com>
2024-03-01 19:53:42 +0530

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,45 @@
+body {
+    margin: 0;
+    padding: 0;
+}
+
+table, tr, td {
+    width: 800px;
+    margin: auto;
+    padding: 5px;
+    text-align: left;
+    border: 2px solid black;
+    border-collapse: collapse;
+    align-items: center;
+}
+
+th {
+    text-align: center;
+    font-size: 30px;
+    background-color: rgb(119, 176, 5);
+    color: white;
+}
+
+td:nth-child(6n+2) {
+    background-color: rgb(179, 230, 103);
+}
+
+td:nth-child(6n+1) {
+    background-color: rgb(218, 241, 171);
+}
+
+td:nth-child(6n+3) {
+    background-color: rgb(218, 241, 171);
+}
+
+td:nth-child(6n+4) {
+    background-color: rgb(179, 230, 103);
+}
+
+td:nth-child(6n+5) {
+    background-color: rgb(218, 241, 171);
+}
+
+td:nth-child(6n+6) {
+    background-color: rgb(179, 230, 103);
+}
